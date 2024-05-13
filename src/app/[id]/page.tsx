b5d8bf720bb422e7ff060f4088346bb6a5c7ecd0
PrajwalLuitel <prajwalluitel@gmail.com>
2024-05-13 16:22:47 -0400

    minor appearance changes

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -25,24 +25,26 @@ export default function landing({ params }: any) {
   }, []);
 
   return (
-    <div className="bg-gray-100">
+    <>
       <nav className="bg-teal-700 text-white text-center py-4">
         <div className="text-3xl font-bold">{params.id}</div>
       </nav>
 
-      <img
-        className="w-1/5 h-1/3 mx-auto mt-4 rounded-md shadow-md"
-        src={image}
-        alt="Sunset Banner"
-      />
+      <div className="flex items-center justify-between p-10 max-md:flex-col">
+        <img
+          className="flex-1 w-2/5 h-1/3 mx-auto mt-4 rounded-2xl max-md:w-4/5 translate-x-11 max-md:-translate-x-0 max-lg:translate-x-2 -z-10 shadow-2xl"
+          src={image}
+          alt="Sunset Banner"
+        />
 
-      <div className="p-8 w-3/4 mx-auto text-justify mb-20">
-        <p>{text}</p>
+        <div className="flex-1 py-[5%] px-4 bg-gray-100 text-teal-900 rounded-3xl w-3/4 max-md:w-full max-md:text-sm text-justify mb-20 shadow-2xl">
+          <p>{text}</p>
+        </div>
       </div>
 
       <footer className="bg-teal-700 text-white text-center py-4 fixed bottom-0 w-full">
         <p>Thank you for Visiting!!</p>
       </footer>
-    </div>
+    </>
   );
 }
