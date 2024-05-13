a171d95ebbe0e66c3067cbf97fa69e992abe81e1
PrajwalLuitel <prajwalluitel@gmail.com>
2024-05-13 14:13:04 -0400

    code refactored

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -3,44 +3,6 @@
 import { useState, useEffect } from "react";
 import { get_description, get_image } from "@/utils";
 
-const styles = {
-  navbar: {
-    textAlign: "center",
-    padding: "1em",
-    background: "#025043",
-    color: "#fff",
-    fontWeight: "bolder",
-    fontSize: "2.5rem",
-  },
-  content: {
-    padding: "2em",
-    width: "70%",
-    margin: "0 auto",
-    textAlign: "justify",
-    marginBottom: "5rem",
-  },
-
-  footer: {
-    padding: "1em",
-    position: "fixed",
-    left: "0",
-    bottom: "0",
-    width: "100%",
-    background: "#025043",
-    color: "#fff",
-    textAlign: "center",
-  },
-
-  banner: {
-    width: "20%",
-    height: "30%",
-    margin: "0 auto",
-    textAlign: "right",
-    borderRadius: "1rem",
-    marginTop: "1rem",
-  },
-};
-
 export default function landing({ params }: any) {
   const [text, setText] = useState("");
   const [image, setImage] = useState("");
@@ -63,19 +25,23 @@ export default function landing({ params }: any) {
   }, []);
 
   return (
-    <div>
-      <nav style={styles.navbar}>
-        <div>{params.id}</div>
+    <div className="bg-gray-100">
+      <nav className="bg-teal-700 text-white text-center py-4">
+        <div className="text-3xl font-bold">{params.id}</div>
       </nav>
 
-      <img style={styles.banner} src={image} />
+      <img
+        className="w-1/5 h-1/3 mx-auto mt-4 rounded-md shadow-md"
+        src={image}
+        alt="Sunset Banner"
+      />
 
-      <div style={styles.content}>
+      <div className="p-8 w-3/4 mx-auto text-justify mb-20">
         <p>{text}</p>
       </div>
 
-      <footer style={styles.footer}>
-        <p>Thank you for Visiting !!</p>
+      <footer className="bg-teal-700 text-white text-center py-4 fixed bottom-0 w-full">
+        <p>Thank you for Visiting!!</p>
       </footer>
     </div>
   );
